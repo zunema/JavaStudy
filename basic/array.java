7079a69c113eaa6b44ad5925cdfe6b7eaae2a856
zunema <yxxa.abc@gmail.com>
2021-04-02 07:19:10 +0000

    array.javaの更新

--- basic/array.java
+++ basic/array.java
@@ -2,19 +2,27 @@ public class array{
 
   public static void main(String[] args){
     int[] array = new int[20];
+    int evensum = 0;
+    int evencount = 0;
+    int oddsum = 0;
+    int oddcount = 0;
     for (int i = 0; i < array.length; i++){
       array[i] = (i+1)*5;
       // System.out.println(array[i]);
     }
     for (int num : array){
       if (num % 2 == 0){
-      System.out.println("偶数、" + num);
+        evensum += num;
+        evencount += 1;
       } else {
-      System.out.println("奇数、" + num);
+        oddsum += num;
+        oddcount += 1;
       }
     }
-    // System.out.println(array[18]);
-    // System.out.println(array.length);
+    System.out.println("偶数の合計は = " + evensum);
+    System.out.println("偶数の個数は = " + evencount);
+    System.out.println("奇数の合計は = " + oddsum);
+    System.out.println("奇数の個数は = " + oddcount);
   }
 
 }
