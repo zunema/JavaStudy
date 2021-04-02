6ac95f8c55d41669527feba8c945717278e73d03
zunema <yxxa.abc@gmail.com>
2021-04-02 06:31:08 +0000

    配列の練習

--- basic/array.java
+++ basic/array.java
@@ -4,8 +4,16 @@ public class array{
     int[] array = new int[20];
     for (int i = 0; i < array.length; i++){
       array[i] = (i+1)*5;
-      System.out.println(array[i]);
+      // System.out.println(array[i]);
     }
+    for (int num : array){
+      if (num % 2 == 0){
+      System.out.println("偶数、" + num);
+      } else {
+      System.out.println("奇数、" + num);
+      }
+    }
+    // System.out.println(array[18]);
     // System.out.println(array.length);
   }
 
