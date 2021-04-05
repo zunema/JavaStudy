39eb1bfcc7a55939f318e45c41b14a30700541ff
zunema <yxxa.abc@gmail.com>
2021-04-05 07:45:20 +0000

    Javaの練習

--- basic/OddSum3.java
+++ basic/OddSum3.java
@@ -1,9 +1,15 @@
 public class OddSum3 {
   public static void main(String[] args) {
+    int evensum = 0;
+    int evencount = 0;
     for (int i = 1; i <= 10; i++) {
       if (i % 2 == 0){
-        System.out.println("これは偶数" + i + "です");
+        // System.out.println("これは偶数" + i + "です");
+        evensum += i;
+        evencount += 1;
       }
     }
+    System.out.println("偶数の合計は" + evensum + "です");
+    System.out.println("偶数の個数は" + evencount + "です");
   }
 }
