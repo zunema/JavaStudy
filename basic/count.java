83040cd302958fb1b163202c057cbf97a17582b6
zunema <yxxa.abc@gmail.com>
2021-04-05 07:34:43 +0000

    Javaの練習

---
+++ basic/count.java
@@ -0,0 +1,11 @@
+public class count {
+  public static void main(String[] args) {
+    for (int i = 1; i <= 10; i++) {
+      if (i == 10) {
+        System.out.print(i);
+      } else {
+       System.out.print(i + ",");
+      }
+    }
+  }
+}
