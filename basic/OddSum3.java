f9ca62d9df0b24ee098ed142755c65fa1142ea5d
zunema <yxxa.abc@gmail.com>
2021-04-05 07:38:13 +0000

    javaの練習

---
+++ basic/OddSum3.java
@@ -0,0 +1,9 @@
+public class OddSum3 {
+  public static void main(String[] args) {
+    for (int i = 1; i <= 10; i++) {
+      if (i % 2 == 0){
+        System.out.println("これは偶数" + i + "です");
+      }
+    }
+  }
+}
