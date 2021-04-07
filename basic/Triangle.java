fd20f582ebe50274ff1ca303e33c7ff48f15cd60
zunema <yxxa.abc@gmail.com>
2021-04-07 08:15:49 +0000

    三角形の作り方

---
+++ basic/Triangle.java
@@ -0,0 +1,10 @@
+public class triangle {
+  public static void main(String[] args) {
+    for (int i = 0; i < 5; i++) {
+      for (int i2 = 0; i2 < i+1; i2++) {
+        System.out.print("*");
+      }
+      System.out.print("\n");
+    }
+  }
+}
